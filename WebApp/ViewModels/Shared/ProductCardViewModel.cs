d9d6fa1fa4eff84f80c00f583075268d74486f9b
simon-off <hellosimonoff@gmail.com>
2023-04-20 17:38:43 +0200

    Renamed product related views to reduce mix-ups

---
+++ WebApp/ViewModels/Shared/ProductCardViewModel.cs
@@ -0,0 +1,24 @@
+using WebApp.Models.Entities;
+
+namespace WebApp.ViewModels.Shared;
+
+public class ProductCardViewModel
+{
+    // TODO: Implement if needed
+
+    // public ProductCardViewModel(Product product)
+    // {
+    //     Name = product.Name;
+    //     Description = product.Description;
+    //     Image = product.Image;
+    //     Price = product.Price;
+    //     Categories = product.Categories;
+    // }
+
+    // public string Name { get; set; }
+    // public string Description { get; set; }
+    // public string Image { get; set; }
+    // public decimal Price { get; set; }
+
+    // public List<Category> Categories { get; set; }
+}
