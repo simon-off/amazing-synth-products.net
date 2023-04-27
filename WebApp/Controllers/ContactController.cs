b2bace2bea366dcfe6cffe853ccf1a79a89cc06e
simon-off <hellosimonoff@gmail.com>
2023-04-27 15:51:32 +0200

    Updated contact form functionality and styling

--- WebApp/Controllers/ContactController.cs
+++ WebApp/Controllers/ContactController.cs
@@ -1,10 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
+using WebApp.ViewModels.Contact;
 
 namespace WebApp.Controllers;
 
 public class ContactController : Controller
 {
+    private readonly Contexts.AppDbContext _context;
+    public ContactController(Contexts.AppDbContext context) { _context = context; }
+
     [HttpGet("contact")]
     public IActionResult Index()
     {
@@ -12,9 +18,20 @@ public class ContactController : Controller
     }
 
     [HttpPost("contact")]
-    public IActionResult Index(ContactMessage contactMessage)
+    public async Task<IActionResult> Index(ContactViewModel viewModel)
     {
-        Console.WriteLine("hello");
+        if (ModelState.IsValid)
+        {
+            await _context.ContactMessages.AddAsync(viewModel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("MessageSent");
+        }
+        return View(viewModel);
+    }
+
+    [HttpGet("contact/sent")]
+    public IActionResult MessageSent()
+    {
         return View();
     }
 }
--- WebApp/ViewModels/Contact/ContactViewModel.cs
+++ WebApp/ViewModels/Contact/ContactViewModel.cs
@@ -1,6 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using WebApp.Models;
+
 namespace WebApp.ViewModels.Contact;
 
 public class ContactViewModel
 {
+    [Required(ErrorMessage = "{0} is required")]
+    public string Name { get; set; } = null!;
 
+    [Required(ErrorMessage = "{0} is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address")]
+    [RegularExpression(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$", ErrorMessage = "Invalid email address")]
+    public string Email { get; set; } = null!;
+
+    [Required(ErrorMessage = "{0} is required")]
+    public string Subject { get; set; } = null!;
+
+    [Required(ErrorMessage = "{0} is required")]
+    public string Message { get; set; } = null!;
+
+    public static implicit operator ContactMessage(ContactViewModel vm)
+    {
+        return new ContactMessage
+        {
+            Name = vm.Name,
+            Email = vm.Email,
+            Subject = vm.Subject,
+            Message = vm.Message,
+        };
+    }
 }
